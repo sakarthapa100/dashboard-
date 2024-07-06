3e95e79cf1cfc7db04c556568366b53f8850addb
hattori100000 <clownlaugh100@gmail.com>
2024-07-06 20:22:15 +0545

    loading..

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import Search from '@/app/ui/dashboard/search/search'
+import Link from 'next/link'
 
-const page = () => {
+const Page = () => {
   return (
-    <div> 
-<div>
-  <table>
-    
-  </table>
-</div>
+    <div>
+      <div className='flex  items-center'>
+        <Search placeholder={"search for the users ..."} />
+<Link href="/dashboard/users/add" >
+<button>Add New</button></Link>
+
+      </div>
+      <table>
+      </table>
     </div>
   )
 }
 
-export default page
+export default Page
---
+++ app/ui/dashboard/search/search.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { MdSearch } from 'react-icons/md';
+
+const search = ({placeholder}) => {
+  return (
+    <div className='flex items-center p-1 border-r-2 border-gray-700 bg-gray-700 w-[19rem]  '>
+    <MdSearch />
+    <input 
+      type="text" 
+      placeholder={placeholder} 
+      className="bg-transparent border-none focus:outline-none text-gray-100 placeholder-gray-400 p-1 w-[19rem] rounded-lg "
+    />
+  </div>
+  )
+}
+
+export default search
